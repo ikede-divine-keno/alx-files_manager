8751300f638242976ddb975daee66f4769c38fe8
ikede-divine-keno <ikededivine@gmail.com>
2024-03-17 12:50:31 +0000

    controllers/AppController.js, controllers/AuthController.js

---
+++ controllers/AppController.js
@@ -0,0 +1,17 @@
+// eslint-disable-next-line import/no-named-as-default
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+class AppController {
+  static getStatus(request, response) {
+    response.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
+  }
+
+  static async getStats(request, response) {
+    const usersNum = await dbClient.nbUsers();
+    const filesNum = await dbClient.nbFiles();
+    response.status(200).json({ users: usersNum, files: filesNum });
+  }
+}
+
+module.exports = AppController;
